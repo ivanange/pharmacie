3e72a27d05d39e0fbdeacf735bfba812ed048490
ivanange <ivanangethe1@gmail.com>
2019-12-27 22:46:13 +0100

    setup models

--- app/Command.php
+++ app/Command.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Command extends Model
 {
+
+    const ENCOURS = 1;
+    const LIVRER = 2;
+    const  ANNULER = 3;
     public $timestamps = false;
 
     public function products()
